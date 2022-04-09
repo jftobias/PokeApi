918df291daa838499d8dd475069d6db05dddb7af
José F. Tobías <jftobiasb@gmail.com>
2022-04-09 03:07:58 -0500

    add endpoint to api/pokemons

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -22,7 +22,8 @@ class PokemonsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions
   def set_pokemon
-    @pokemon = Pokemon.friendly.find(params[:id])  end
+    @pokemon = Pokemon.friendly.find(params[:id])
+  end
 
   # Only allow a list of trusted parameters through.
   def pokemon_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :pokemons
   root "welcome#index"
+  namespace :api do
+    resources :pokemons
+  end
 end
